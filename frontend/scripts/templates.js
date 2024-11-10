ab9c0c3ee93b91c63a1e25a804b6bb72e1c0af79
Vyacheslav Shaganov <vyacheslav.shaganov@yandex.ru>
2024-11-10 13:55:23 +0300

    main page styled properly

--- frontend/scripts/templates.js
+++ frontend/scripts/templates.js
@@ -5,19 +5,32 @@ function normalizeRating(rating) {
     return ''+d1+','+d2;
 }
 
+function addClass(html_string, classname, selector='advanced-search-card') {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html_string, 'text/html');
+    
+    let els = doc.getElementsByClassName(selector);
+    for (let index = 0; index < els.length; index++)
+        els[index].classList.add(classname);
+
+    return doc.documentElement.innerHTML;
+}
+
+function addColumnClass(card) {
+    return addClass(card, 'col-md-6');
+}
+
 function generateCourtyardCard(label, address, rating) {
     rating = normalizeRating(rating);
-    return `<div class="col-md-6 mb-3 advanced-search-card">
+    return `<div class="mb-3 advanced-search-card">
                 <div class="list-group-item">
                     <div class="d-flex justify-content-between align-items-center">
                         <div>
-                            <h6>`+label+`</h6>
-                            <p class="mb-0 text-muted">`+address+`</p>
-                            <small class="text-warning">★ `+rating+`</small>
+                            <h6>${label}</h6>
+                            <p class="mb-0 text-muted">${address}</p>
+                            <small class="text-warning">★ ${rating}</small>
                         </div>
-                        <a href="#" class="btn btn-outline-secondary btn-sm">
-                            <i class="fas fa-external-link-alt"></i>
-                        </a>
+                        <a href="courtyard.html?id=123" class=""><span class="material-icons purple f-24">open_in_new</span></a>
                     </div>
                 </div>
             </div>`
@@ -25,13 +38,13 @@ function generateCourtyardCard(label, address, rating) {
 
 function generateUserCard(username, last_name, first_name, patronymic, visited) {
 
-    return `<div class="col-md-6 mb-3 advanced-search-card">
+    return `<div class="mb-3 advanced-search-card">
                 <div class="list-group-item d-flex justify-content-between align-items-center">
                     <span>
-                        <span class="card-username">`+username+`</span> 
-                        <span class="card-aka">aka `+last_name+" "+first_name+" "+patronymic+" "+`</span>
+                        <span class="card-username">${username}</span> 
+                        <span class="card-aka">aka ${last_name} ${first_name} ${patronymic}</span>
                         <br>
-                        <small class="text-danger">`+visited+` двора</small>
+                        <small class="text-danger">${visited} двора</small>
                     </span>
                     <a href="#" class=""><span class="material-icons purple f-24">open_in_new</span></a>
                 </div>
