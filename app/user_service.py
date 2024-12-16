53097a85d5d924c6ea5d207f7537ec9e4902c114
Ilya Belokobylsky <ilya.belokobylsky@gmail.com>
2024-12-16 15:51:56 +0300

    Update user_service.py

--- app/user_service.py
+++ app/user_service.py
@@ -82,6 +82,7 @@ def get_user_by_id(user_id: str):
         """,
         {"user_id": user_id}
     )
+    
     if not result or len(result) == 0:
         raise HTTPException(status_code=404, detail="User not found")
     return result[0]
